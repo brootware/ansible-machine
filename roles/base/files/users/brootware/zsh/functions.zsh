7e3dbb11d463dc8e976bae3a02df0322e8ebc202
Bruce <brootware@machine>
2026-07-02 22:39:46 +0800

    added useful functions

--- roles/base/files/users/brootware/zsh/functions.zsh
+++ roles/base/files/users/brootware/zsh/functions.zsh
@@ -1,3 +1,20 @@
+## Functions
+
+# Show a directory listing when using 'cd'
+function cd() {
+    new_directory="$*";
+    if [ $# -eq 0 ]; then
+        new_directory=${HOME};
+    fi;
+    builtin cd "${new_directory}" && /bin/ls -lhF --time-style=long-iso --color=auto --ignore=lost+found
+}
+
+# Make a directory, then go there
+function md() {
+    test -n "$1" || return
+    mkdir -p "$1" && cd "$1"
+}
+
 # Easily extract archives
 extract () {
   if [ -f $1 ] ; then
