97f4e57ae605e9ef69317f92efdeeff69c05259f
Bruce <brootware@machine>
2026-07-03 08:17:50 +0800

    keeping prompt consistent for zsh

--- roles/base/files/users/brootware/zsh/prompt.zsh
+++ roles/base/files/users/brootware/zsh/prompt.zsh
@@ -1,3 +1,7 @@
-# Set the prompt
-newline=$'\n'
-prompt='%F{35}%* [%j]${git_prompt} [%m:%F{75}%f%F{69}%c%f%F{35}] %#%f ${newline}👓 '
+# Colors
+C35='\[\e[38;5;35m\]'   # accents
+C38='\[\e[38;5;38m\]'   # values
+RST='\[\e[0m\]'
+
+# Prompt
+export PS1="${RST}${C35}╭─(${C38}\t${C35})-(${C38}\j${C35})-(${C38}\H${C35})-(${C38}\w${C35})\n${C35}╰──🚀 ${RST}"
