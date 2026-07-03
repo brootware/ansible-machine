d8166ba14f921017224b74232aa0c36686492f06
Bruce <brootware@machine>
2026-07-03 09:35:19 +0800

    updated root prompt

--- roles/base/files/users/brootware/zsh/prompt.zsh
+++ roles/base/files/users/brootware/zsh/prompt.zsh
@@ -4,4 +4,5 @@ C38='\[\e[38;5;38m\]'   # values
 RST='\[\e[0m\]'
 
 # Prompt
-export PS1="${RST}${C35}╭─(${C38}\t${C35})-(${C38}\j${C35})-(${C38}\H${C35})-(${C38}\w${C35})\n${C35}╰──🚀 ${RST}"
+newline=$'\n'
+prompt='%F{35}%* [%j]${git_prompt} [%m:%F{75}%f%F{69}%c%f%F{35}] %#%f ${newline}╰──🚀 '
