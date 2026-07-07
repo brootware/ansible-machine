d4a46004e198864eb1c14c9ef4879e5d77cd597d
Bruce <brootware@machine>
2026-07-07 16:55:53 +0800

    added more functions

--- roles/base/files/windows/functions.ps1
+++ roles/base/files/windows/functions.ps1
@@ -13,3 +13,16 @@ function Edit-Dotfiles {
 function Extract-Archive ($Path) {
     Expand-Archive -Path $Path -DestinationPath . -Force
 }
+
+function Find-File($name) {
+    ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
+        $place_path = $_.directory
+        echo "${place_path}\${_}"
+    }
+}
+
+function Edit-Hosts { code $env:windir\System32\Drivers\etc\hosts }
+
+function Get-Pubip {(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content}
+
+function Get-Routerip {(Get-NetRoute | Where-Object { $_.DestinationPrefix -eq '0.0.0.0/0' }).NextHop}
